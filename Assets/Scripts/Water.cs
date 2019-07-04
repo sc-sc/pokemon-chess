2b88edad2ea4b8d4f5561dc1c71d203ed549b271
BreadKey <t9rres@ajou.ac.kr>
2019-07-04 21:13:44 +0900

    맵 타일 및 물 깔아놓음

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y >= 2f) {
+            time = 0f;
+            transform.position = new Vector3(2f + transform.position.x, 2f - transform.position.y);
+        } else
+        {
+            time += Time.deltaTime / 10f;
+            transform.position = Vector2.Lerp(new Vector2(0f, 0f), new Vector2(-2f, 2f), time);
+        }
+    }
+}
