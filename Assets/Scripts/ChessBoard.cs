adde6b7e4d2b8a2c9d211214176ff37d42d94544
BreadKey <t9rres@ajou.ac.kr>
2019-07-07 12:38:53 +0900

    격자 아예 마름모 형태로 변경, 터치 이동 구현

---
+++ Assets/Scripts/ChessBoard.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ChessBoard : MonoBehaviour, Touchable
+{
+    private Tilemap tilemap;
+    private Vector3Int selectedPosition;
+    private Vector3Int passingPosition;
+    private Pokemon selectedPokemon;
+
+    public void Moved(Vector3 to)
+    {
+        if (passingPosition != null && passingPosition != selectedPosition)
+        {
+            tilemap.SetColor(passingPosition, Color.white);
+        }
+
+        passingPosition = tilemap.WorldToCell(to);
+        if (passingPosition != selectedPosition)
+        {
+            tilemap.SetColor(passingPosition, Color.green);
+        }
+            
+        if (selectedPokemon != null)
+        {
+            selectedPokemon.transform.position = to;
+        }
+    }
+
+    public void Released(Vector3 at)
+    {
+        tilemap.SetColor(selectedPosition, Color.white);
+        tilemap.SetColor(passingPosition, Color.white);
+
+        Debug.Log(passingPosition);
+        ChessSquare chessSquare = tilemap.GetTile(tilemap.WorldToCell(passingPosition)) as ChessSquare;
+
+        if (!PlacePokemon(passingPosition, selectedPokemon))
+        {
+            selectedPokemon.transform.position = tilemap.GetCellCenterWorld(selectedPosition);
+        }
+
+        selectedPokemon = null;
+    }
+
+    public void Touched(Vector3 at)
+    {
+        selectedPosition = tilemap.WorldToCell(at);
+        tilemap.SetColor(selectedPosition, Color.cyan);
+
+        selectedPokemon = (tilemap.GetTile(selectedPosition) as ChessSquare)?.GetLocatedPokemon();
+    }
+
+    public bool PlacePokemon(Vector3Int position, Pokemon pokemon)
+    {
+        ChessSquare chessSquare = tilemap.GetTile(position) as ChessSquare;
+        if (chessSquare == null)
+        {
+            return false;
+        }
+
+        chessSquare.LocatePokemon(pokemon);
+        pokemon.transform.position = tilemap.GetCellCenterWorld(position);
+
+        return true;
+    }
+
+    void Awake()
+    {
+        tilemap = GetComponent<Tilemap>();
+
+        PlacePokemon(new Vector3Int(-1, 0, 0), FindObjectOfType<Pokemon>());
+    }
+}
---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    public int maxTouchCount;
+    private Touchable[] touchedObjects;
+    private bool isMouseButtonDowned = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        touchedObjects = new Touchable[5];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMouseButtonDowned = true;
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            touchDown(hit, 0);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isMouseButtonDowned = false;
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            touchUp(hit.point, 0);
+        }
+
+        if (isMouseButtonDowned)
+        {
+            touchMove(Camera.main.ScreenToWorldPoint(Input.mousePosition), 0);
+        }
+
+        for(int index = 0; index < Input.touchCount; index++)
+        {
+            Touch touch = Input.GetTouch(index);
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(touch.position), Vector2.zero);
+            
+            switch(touch.phase)
+            {
+                case TouchPhase.Began:
+                    touchDown(hit, index);
+                    break;
+                case TouchPhase.Moved:
+                    touchMove(hit.point, index);
+                    break;
+                default:
+                    touchUp(hit.point, index);
+                    break;
+            }
+        }
+    }
+
+    private void touchDown(RaycastHit2D hit, int index)
+    {
+        Touchable touchedObject = hit.collider?.gameObject.GetComponent<Touchable>();
+        touchedObjects[index] = touchedObject;
+
+        touchedObject?.Touched(hit.point);
+    }
+
+    private void touchMove(Vector2 to, int index)
+    {
+        touchedObjects[index]?.Moved(to);
+    }
+
+    private void touchUp(Vector2 at, int index)
+    {
+        touchedObjects[index]?.Released(at);
+        touchedObjects[index] = null;
+    }
+}
