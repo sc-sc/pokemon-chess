4dfa02119ada273ee7a7c8402dc05ff70491afa4
kh06089 <kh06089@ajou.ac.kr>
2019-07-20 18:58:52 +0900

    사용자 정보 표시 변경

---
+++ Assets/Scripts/TrainerInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrainerInfo : MonoBehaviour
+{
+    public Button goChessFieldButton;
+    public Text nickname;
+    [SerializeField]
+    private Text level;
+    [SerializeField]
+    private Image hpGauge;
+    public Image Icon;
+
+    public void SetHp(int hp, int currentHp)
+    {
+        hpGauge.fillAmount = (float) currentHp / hp;
+    }
+
+    public void SetLevel(int level)
+    {
+        this.level.text = "Lv." + level.ToString();
+    }
+
+    public void SetHpColor(Color color)
+    {
+        hpGauge.color = color;
+    }
+}
---
+++ Assets/Scripts/TrainerList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainerList : MonoBehaviour
+{
+    public Color[] trainerColor;
+
+    public GameManager gm;
+    public GameObject layout;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int index = 0;
+        foreach (Trainer trainer in gm.trainers)
+        {
+            TrainerInfo trainerInfo = Instantiate(Resources.Load("Prefabs/TrainerInfo") as GameObject, layout.transform).GetComponent<TrainerInfo>();
+            trainerInfo.nickname.text = trainer.nickname;
+            trainerInfo.SetLevel(trainer.level);
+            trainerInfo.SetHp(trainer.hp, trainer.currentHp);
+            trainerInfo.SetHpColor(trainerColor[index]);
+            index++;
+        }
+    }
+ }
